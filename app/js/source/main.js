63c0d6ee3c38c9158ff9b826eb0ea3c753d4d88e
JamesNix <jamesfnix@gmail.com>
2014-03-25 14:34:52 -0500

    startoff branch working

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -12,48 +12,43 @@
 
   }
 
-  var imgArray = [];
-  var linkArray = [];
-  var tagArray = [];
+  // var imgArray = [];
+  // var linkArray = [];
+  // var tagArray = [];
 
   function getPhotos(){
-    debugger;
-    var $tags = $('#tags').text();
-    $.ajax({
-      type: 'GET',
-      dataType: 'jsonp',
-      cache: false,
-      url: 'https://api.instagram.com/v1/tags/' + $tags + '/media/recent?client_id=bc1a82e4c61a4ecbb3be6f354b2aba8f',
-      success: function(data) {
-        //console.log(data);
-        for (var i = 0; i < 9; i++) {
-          var $div = $('<div>').css('background-image', 'url('+data.data[i].images.thumbnail.url+')').addClass('frame').attr('id', 'frame' + [i]);
-          $div.wrap('<a target="blank" href="' + data.data[i].link + '"><a/>');
-          $('#target').append($div);
+    if($('.frame').length === 0){
+      var tags = $('#tags').val();
+      $.ajax({
+        type: 'GET',
+        dataType: 'jsonp',
+        cache: false,
+        url: 'https://api.instagram.com/v1/tags/' + tags + '/media/recent?client_id=bc1a82e4c61a4ecbb3be6f354b2aba8f',
+        success: function(data){
+          for(var i = 0; i < 9; i++){
+            var $div = $('<div>').css('background-image', 'url('+data.data[i].images.thumbnail.url+')').addClass('frame').attr('id', 'frame' + [i]);
+            $div.wrap('<a target="blank" href="' + data.data[i].link + '"></a>');
+            $('#target').append($div);
 
-
-          imgArray.push(data.data[i].images.thumbnail.url);
-          linkArray.push(data.data[i].link);
-          tagArray.push(data.data[i].tags);
+          // imgArray.push(data.data[i].images.thumbnail.url);
+          // linkArray.push(data.data[i].link);
+          // tagArray.push(data.data[i].tags);
 
           // console.log(imgArray);
           // console.log(linkArray);
           // console.log(tagArray);
+          }
         }
-      }
-    });
-    randomizer();
+      });
+      randomizer();
+    }
   }
 
   function randomizer() {
-    debugger;
     var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    var counter = 0;
+    // var counter = 0;
     (function numShuffle(){
-      counter++;
-      // if(counter > 8) {
-      //   return;
-      // }
+      // counter++;
       var random = _.shuffle(nums);
       var random1 = random[3];
       var random2 = random[6];
@@ -70,11 +65,13 @@
       setTimeout(numShuffle, 3000);
     })();
   }
-
-  // function imgFade() {
-  //   //$('#target .frame').first().appendTo('#target').fadeOut(10000);
-  //   $('#target .frame').first().fadeIn(10000);
-  //   setTimeout(imgFade, 14000);
-  // }
-  // imgFade();
 })();
+
+  // function getTweets(){
+  //
+  //   $.ajax({
+  //     url : "",
+  //     dataType : "json",
+  //     timeout : 15000,
+  //   });
+  // }
